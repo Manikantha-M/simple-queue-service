ae0d26b628c380ec9ea7d1316745f3d1c8031eaf
Manikantha M <manikantha.m@neutrinos.co>
2026-02-20 12:40:58 +0300

    simple queue service set up

--- lib/simple-queue-service-stack.ts
+++ lib/simple-queue-service-stack.ts
@@ -1,16 +1,46 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambdaBase from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
 export class SimpleQueueServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const queue = new sqs.Queue(this, 'OrdersQueue', {
+      visibilityTimeout: cdk.Duration.seconds(30),
+      queueName:`${this.stackName}-orders-queue`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleQueueServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const producerLambda = new NodejsFunction(this, 'OrderProducer', {
+      runtime:lambdaBase.Runtime.NODEJS_22_X,
+      entry:path.join(__dirname, '../src/lambda/handler.ts'),
+      handler:'producer',
+      functionName:`${this.stackName}-producer`,
+      environment:{
+        QUEUE_URL: queue.queueUrl,
+      }
+    });
+    queue.grantSendMessages(producerLambda);
+    const consumerLambda = new NodejsFunction(this, 'OrderConsumer', {
+      runtime:lambdaBase.Runtime.NODEJS_22_X,
+      entry:path.join(__dirname, '../src/lambda/handler.ts'),
+      handler:'consumer',
+      functionName:`${this.stackName}-consumer`
+    });
+
+    consumerLambda.addEventSource(new lambdaEventSources.SqsEventSource(queue, {
+      batchSize: 1
+    }))
+    const api = new apigateway.RestApi(this, 'OrdersAPI');
+    const orders = api.root.addResource('orders');
+    orders.addMethod('POST', new apigateway.LambdaIntegration(producerLambda));
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: api.url
+    })
   }
 }
